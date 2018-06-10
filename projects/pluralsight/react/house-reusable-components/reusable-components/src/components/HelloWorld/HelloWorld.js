115392c6432f5fa9a55777be6217d63da46cb138
Schmalte <pages.ohoolahan@gmail.com>
2018-06-10 23:24:44 +0200

    projects reusable react components finish up doc gen enviroment

--- projects/pluralsight/react/house-reusable-components/reusable-components/src/components/HelloWorld/HelloWorld.js
+++ projects/pluralsight/react/house-reusable-components/reusable-components/src/components/HelloWorld/HelloWorld.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
+/** Discription for Component */
 const HelloWorld = ({message}) => {
     return <div>Hello {message} </div>
 }
 
 HelloWorld.propTypes = {
+    /** Discription for prop */
     message: PropTypes.string
 };
+
+HelloWorld.defaultProps = {
+    message: 'world',
+}
 
 export default HelloWorld;
---
+++ projects/pluralsight/react/house-reusable-components/reusable-components/src/docs/examples/HelloWorld/ExampleHelloWorld.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import HelloWorld from 'ps-react/HelloWorld'
+
+/** Example Descripition Component Hello World */
+const ExampleHelloWorld = () => {
+    return <HelloWorld message="Dies das ananas" />
+}
+
+export default ExampleHelloWorld;
