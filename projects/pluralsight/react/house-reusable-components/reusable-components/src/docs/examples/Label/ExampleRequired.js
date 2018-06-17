206e4357395417a2eb0be1106ccc650e58d2f1fc
Schmalte <pages.ohoolahan@gmail.com>
2018-06-17 18:52:41 +0200

    project reuseable components did some atoms

---
+++ projects/pluralsight/react/house-reusable-components/reusable-components/src/docs/examples/Label/ExampleRequired.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Label from 'ps-react/Label';
+
+/** required label */
+export default function ExampleOptional(){
+    return <Label htmlFor="test" label="test" required/>
+}
