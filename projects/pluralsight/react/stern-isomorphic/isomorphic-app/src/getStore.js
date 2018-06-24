a046bb90f26398e6648982dd20d0cb214cd635d0
Schmalte <pages.ohoolahan@gmail.com>
2018-06-25 00:17:10 +0200

    project isomorphic react start client

---
+++ projects/pluralsight/react/stern-isomorphic/isomorphic-app/src/getStore.js
@@ -0,0 +1,8 @@
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import { identity } from 'lodash';
+
+export default function(defaultState = {
+    test:"Test Value"
+}){
+    return createStore(identity, defaultState);
+}
---
+++ projects/pluralsight/react/stern-isomorphic/isomorphic-app/src/index.jsx
@@ -0,0 +1,18 @@
+import App from './App';
+import ReactDom from 'react-dom';
+import React from 'react';
+import getStore from './getStore';
+import { Provider } from 'react-redux';
+
+const store = getStore();
+
+const render = (_App) => {
+    ReactDom.render(
+        <Provider store={store}>
+            <_App />
+        </Provider>,
+        document.getElementById('AppContainer')
+    );
+};
+
+render(App);
